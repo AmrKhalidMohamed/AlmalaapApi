fd1bad22baccee75e9b313cf1929baa5d59352a8
Amr Khaled <amrkfawzy@gmail.com>
2024-04-03 03:24:51 +0200

    retest

--- resources/views/pages/rooms/index.blade.php
+++ resources/views/pages/rooms/index.blade.php
@@ -42,7 +42,7 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12 mt-3">
-                            <input type="submit" class="btn btn-danger" value="Add">
+                            <input type="submit" class="btn btn-primary" value="Add">
                         </div>
 
                     </div>
